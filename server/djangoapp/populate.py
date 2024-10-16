12d989619c799d99e563a85033d022e386518edb
Christopher578 <160020674+Christopher578@users.noreply.github.com>
2024-10-16 12:50:15 +1100

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -34,8 +34,8 @@ def initiate():
       {"name": "Cerato", "type": "Sedan", "year": 2023, "car_make": car_make_instances[3]},
       {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Camry", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
-      {"name": "Kluger", "type": "SUV", "year": 2023, "car_make": car_make_inst
-       ances[4]},
+      {"name": "Kluger", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[4]},
         # Add more CarModel instances as needed
     ]
 
