e3821ab3cb3e337571a41384b1fe491ce40f0fca
Christopher578 <160020674+Christopher578@users.noreply.github.com>
2024-10-17 20:17:10 +1100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,5 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 
@@ -11,7 +10,6 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
